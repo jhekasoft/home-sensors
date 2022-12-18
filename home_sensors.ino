832b586aef8d7790b0b56decc86089020800fe18
Eugene Efremov <jhekasoft@gmail.com>
2022-12-18 09:57:00 +0200

    Add ability to select sensor

--- home_sensors.ino
+++ home_sensors.ino
@@ -2,10 +2,13 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-#include <DHT_U.h>
-#include <DHT.h>
+#ifdef BME280_USE
+  #include <Adafruit_Sensor.h>
+  #include <Adafruit_BME280.h>
+#elif DHT11_USE
+  #include <DHT_U.h>
+  #include <DHT.h>
+#endif
 
 char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = WIFI_SSID;
@@ -13,8 +16,11 @@ char pass[] = WIFI_PASSWORD;
 
 BlynkTimer timer;
 
-Adafruit_BME280 bme; // I2C BME
-DHT dht(DHT_PIN, DHT11); // DHT
+#ifdef BME280_USE
+  Adafruit_BME280 bme; // I2C BME
+#elif DHT11_USE
+  DHT dht(DHT_PIN, DHT11); // DHT
+#endif
 
 void setup() {
   Serial.begin(115200);
@@ -29,17 +35,23 @@ void setup() {
   //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
   //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
 
-  // BME sensor setup
-  bool bmeStatus = bme.begin(BME280_ADDRESS_ALTERNATE);
-  if (!bmeStatus) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-  }
+  #ifdef BME280_USE
+    // BME sensor setup
+    bool bmeStatus = bme.begin(BME280_ADDRESS_ALTERNATE);
+    if (!bmeStatus) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    }
+  #elif DHT11_USE
+    // DHT sensor setup
+    dht.begin();
+  #endif
 
-  // DHT sensor setup
-  dht.begin();
+  // Custom connected event
+  Blynk.logEvent("connected");
 
   // Setup a function to be called every minute
   timer.setInterval(60000L, publishTimerEvent);
+  publishTimerEvent();
 }
 
 void loop() {
@@ -49,19 +61,24 @@ void loop() {
 
 void publishTimerEvent() {
   // Values
-  float temperature = bme.readTemperature();
-  float pressure = bme.readPressure() / 100.0F;
-  float humidity = bme.readHumidity();
-  float temperature2 = dht.readTemperature();
-  float humidity2 = dht.readHumidity();
+  #ifdef BME280_USE
+    float temperature = bme.readTemperature();
+    float humidity = bme.readHumidity();
+    float pressure = bme.readPressure() / 100.0F;
+  #elif DHT11_USE
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+    float pressure = 0;
+  #endif
 
   // Publish to Blynk
   Serial.println("Publishing to Blynk...");
   Blynk.virtualWrite(V1, temperature); // For Temperature
   Blynk.virtualWrite(V2, pressure); // For Pressure
   Blynk.virtualWrite(V3, humidity); // For Humidity
-  Blynk.virtualWrite(V4, temperature2); //For Temperature 2
-  Blynk.virtualWrite(V5, humidity2); // For Humidity 2
+  // TODO: Remove. Legacy
+  Blynk.virtualWrite(V4, 0); // For Temperature 2
+  Blynk.virtualWrite(V5, 0); // For Humidity 2
 
   Serial.println();
 }
